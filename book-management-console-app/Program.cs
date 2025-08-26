9ff1d5c94293c2dccedd5a94f2e0e3516e17872a
VincentB <vincent.brunelle.gm@gmail.com>
2025-08-26 14:50:37 -0400

    ADD: Genre enum and Author class

--- book-management-console-app/Program.cs
+++ book-management-console-app/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Book management program running...");
+﻿namespace BookManagementConsoleApp
+{
+    enum Genre
+    {
+        Thriller,
+        Fantasy,
+        ScienceFiction,
+        Horror,
+        Romance,
+        Historical,
+        Biography,
+        History,
+        Encyclopedia,
+
+    }
+
+    class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string LastName { get; set; }
+        public string Nationality { get; set; }
+        public string Biography { get; set; }
+        
+        public Author(int id, string name, DateOnly birthDate, string lastName, string nationality, string biography)
+        {
+            Id = id;
+            Name = name;
+            BirthDate = birthDate;
+            LastName = lastName;
+            Nationality = nationality;
+            Biography = biography;
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Book management program running...");
+        }
+    }
+}
